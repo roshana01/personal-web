8022a4a0c4e49f7893bf9ce843e67daf6c903645
roshana01 <roshanaroshani99@gmail.com>
2023-01-30 17:30:39 +0330

    Update : component app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Landing from './Components/Landing/Landing'
+import AboutMe from './Components/AboutMe/AboutMe'
+import CounterServices from './Components/CounterServices/CounterServices'
 
 export default function App() {
   return (
     <div>
       <Landing />
+      <AboutMe />
+      <CounterServices />
     </div>
   )
 }
