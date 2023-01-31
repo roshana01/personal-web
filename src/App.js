da143fcf9860e31527072e70d89c0db76e76b757
roshana01 <roshanaroshani99@gmail.com>
2023-01-31 20:26:12 +0330

    Add : Main page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import Landing from './Components/Landing/Landing'
-import AboutMe from './Components/AboutMe/AboutMe'
-import CounterServices from './Components/CounterServices/CounterServices'
+import Main from './Pages/Main/Main'
 
 export default function App() {
   return (
     <div>
-      <Landing />
-      <AboutMe />
-      <CounterServices />
+      <Main />
     </div>
   )
 }
--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './Main.css'
+import Landing from '../../Components/Landing/Landing'
+import Slider from '../../Components/Slider/Slider'
+import AboutMe from '../../Components/AboutMe/AboutMe'
+import CounterServices from '../../Components/CounterServices/CounterServices'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <>
+      <Landing />
+      <AboutMe />      
+      <CounterServices />
+      <Slider />
+    </>
   )
 }
+
+
