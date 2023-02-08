8915c22d0e217310ca80a09aef28acc7a084af55
roshana01 <roshanaroshani99@gmail.com>
2023-02-08 20:42:29 +0330

    Add

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -1,58 +1,28 @@
 import React from 'react'
+import { skills } from '../../data/data'
 import SkillProgress from '../SkillProgress/SkillProgress'
+
 import './AboutMe.css'
 
-
-import { CiBasketball } from 'react-icons/ci';
-import { RiGlobalLine } from 'react-icons/ri';
-import { HiOutlineCamera } from 'react-icons/hi';
-import { SiSourcegraph } from 'react-icons/si';
 export default function AboutMe() {
-
-  // let SkillProgres = [
-  //   { icon: <ImMagicWand />, title: "UI Interface Design", percent: "20%" },
-  //   { icon: <CiBasketball />, title: "UI Interface Design", percent: "40%" },
-  //   { icon: <RiGlobalLine />, title: "UI Interface Design", percent: "50%" },
-  //   { icon: < HiOutlineCamera />, title: "UI Interface Design", percent: "60%" },
-  //   { icon: < SiSourcegraph />, title: "UI Interface Design", percent: "10%" },
-  // ]
-
   return (
-    <div className='about'>
+    <div className='about' id='about'>
       <div className="about-avatar">
         <img src="./images/avator.jpg" alt="" className='about-avatar--img' />
       </div>
 
       <div className="about-resume">
-        <div className='resume-history'>
-          <h1 className='resume-title'>Work Experient</h1>
-          <div className='resume-history-experience'>
-            <h3>front-end</h3>
-            <p>at fourth compn</p>
-            <span>2019-persont</span>
-          </div>
-          <div className='resume-history-experience'>
-            <h3>front-end</h3>
-            <p>at fourth compn</p>
-            <span>2019-persont</span>
-          </div>
-          <div className='resume-history-experience'>
-            <h3>front-end</h3>
-            <p>at fourth compn</p>
-            <span>2019-persont</span>
-          </div>
-          <div className='resume-history-experience'>
-            <h3>front-end</h3>
-            <p>at fourth compn</p>
-            <span>2019-persont</span>
-          </div>
-
-
-        </div>
         <div className='resume-skills'>
           <h1 className='resume-title'>skills Expertise</h1>
-
-          <SkillProgress />
+          {
+            skills.map((item)=>(
+              <SkillProgress title={item.title} percentAge={item.percent} >
+                  {item.icon}
+              </SkillProgress>
+             
+            ))
+          }
+       
         </div>
       </div>
     </div>
